2965b554fbc36848660013f53c4c2a88d2259b75
Laam Wong <laamwong@Laams-iMac.local>
2022-11-14 20:03:51 +0800

    add navbar

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,33 +1,46 @@
 import { Menu, MenuProps, Space } from "antd";
 import React from "react";
 import s from "./Header.module.scss";
-import { CopyrightOutlined } from "@ant-design/icons";
+import {
+  CopyrightOutlined,
+  GithubOutlined,
+  InstagramOutlined,
+  LinkedinOutlined,
+} from "@ant-design/icons";
 import { ItemType } from "antd/lib/menu/hooks/useItems";
 import Link from "next/link";
+import { useMediaQuery } from "react-responsive";
 
 const items: MenuProps["items"] = [
   {
-    label: (
-      <a href="https://www.google.com.au/" target={"_blank"} rel="noreferrer">
-        Projects
-      </a>
-    ),
+    label: <a href="#project">Projects</a>,
     key: "project",
   },
-  { label: "Technologies", key: "technologies" },
+  { label: <a href="#tech">Technologies</a>, key: "technologies" },
+  { label: <a href="#about">About</a>, key: "about" },
 ];
 
 const Header = () => {
+  const isTablet = useMediaQuery({ maxWidth: 1150 });
+  console.log(isTablet);
+
   return (
-    <div className={s.wrapper}>
-      <div className={s.title}>
-        <CopyrightOutlined />
-        Portfolio
+    <div className={s.container}>
+      <div className={s.wrapper}>
+        <div className={s.title}>
+          <CopyrightOutlined className={s.title_icon} />
+          Portfolio
+        </div>
+        <div className={s.menu}>
+          <Menu items={items} mode="horizontal" className={s.menu_items} />
+        </div>
+
+        <div className={s.icons}>
+          <GithubOutlined className={s.icon} />
+          <LinkedinOutlined className={s.icon} />
+          <InstagramOutlined className={s.icon} />
+        </div>
       </div>
-      <div className={s.menu}>
-        <Menu items={items} />
-      </div>
-      <div className={s.icons}></div>
     </div>
   );
 };
